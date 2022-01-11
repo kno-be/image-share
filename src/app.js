8ce8ef25b9c7f01127a25dcdf36ff68f073a61db
berhart <beknoblauch18@gmail.com>
2022-01-11 15:15:00 -0300

    upd:

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 let express = require("express");
 let app = express();
+require('dotenv').config()
+
+const { MongoClient } = require('mongodb');
+const uri = `mongodb+srv://berhart:${process.env.PASSWORD}@cluster0.bho24.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";`
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
--- test/server.test.js
+++ test/server.test.js
@@ -2,12 +2,14 @@ let app = require ("../src/app")
 let supertest = require ("supertest");
 let request = supertest(app);
 
-it("should listen on port 3000", () => {
-
-    return request.get("http://localhost:3000/").then(res=> {
-        let status = res.statusCode
-        expect(status).toEqual(200);
-    }).catch(err => {
-        fail(err);
+describe("Server connections", () => {
+    it("should listen on port 3000", () => {
+    
+        return request.get("http://localhost:3000/").then(res=> {
+            let status = res.statusCode
+            expect(status).toEqual(200);
+        }).catch( () =>  {
+                   console.log("seila") 
+                })
     })
 })
