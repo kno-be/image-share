1b7b0b8076cde795ffcb453301f37f2725d0dc01
berhart <beknoblauch18@gmail.com>
2022-01-11 15:15:20 -0300

    upd: mongoose

--- src/app.js
+++ src/app.js
@@ -1,22 +1,15 @@
 let express = require("express");
 let app = express();
-require('dotenv').config()
-
-const { MongoClient } = require('mongodb');
-const uri = `mongodb+srv://berhart:${process.env.PASSWORD}@cluster0.bho24.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";`
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+require('dotenv').config();
+let mongoose = require("mongoose");
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
+mongoose.connect("mongodb://localhost:27017/pics")
+
 app.get('/', (req, res) => {
-    res.statusCode(200);
+    res.json({});
 })
 
 module.exports = app;
